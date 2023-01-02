ce0777d0a78f4210b972ff1de453326cf0873deb
FirmannSyah <firmann1952@gmail.com>
2023-01-02 19:14:53 +0700

    Update 2.3
    Penambahan menu

--- lib/route/route.dart
+++ lib/route/route.dart
@@ -5,13 +5,14 @@ import 'package:nihongo_learn/view/main_menu.dart';
 import 'package:nihongo_learn/view/wordsheetCourse.dart';
 import 'package:nihongo_learn/view/login_page.dart';
 import 'package:nihongo_learn/view/register_page.dart';
-
+import 'package:nihongo_learn/view/lessonPage.dart';
 
 // route names
 const String homePage = 'homepage';
 const String secondPage = 'secondpage';
 const String thirdPage = 'thirdpage';
 const String Quiz = 'quiz';
+const String Lesson = 'lesson';
 
 void homepage() {}
 
@@ -19,13 +20,15 @@ void homepage() {}
 Route<dynamic> controller(RouteSettings settings) {
   switch (settings.name) {
     case homePage:
-      return MaterialPageRoute(builder: (context) => MyHomePage(title: "nihongo.learn"));
+      return MaterialPageRoute(builder: (context) => MyApp());
     case secondPage:
       return MaterialPageRoute(builder: (context) => wordsheet());
      case thirdPage:
        return MaterialPageRoute(builder: (context) => RegisterPage() );
     case Quiz:
       return MaterialPageRoute(builder: (context) => MainMenu() );
+    case Lesson:
+      return MaterialPageRoute(builder: (context) => lesson() );
     default:
       throw ('this route name does not exist');
   }
